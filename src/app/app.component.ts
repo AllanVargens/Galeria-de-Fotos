b44db26517cd1fd42330ecc878b52ee74fa2ccf9
AllanVargens <allanvargens@gmail.com>
2024-07-01 19:19:20 -0300

    fix: app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CardBarComponent } from './components/card-bar/card-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, CardBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
