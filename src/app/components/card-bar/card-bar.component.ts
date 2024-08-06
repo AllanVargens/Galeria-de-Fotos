3a6c9ebcbcd2dcdf90c1a7c4877b3058f56d111e
AllanVargens <allanvargens@gmail.com>
2024-08-06 13:49:03 -0300

    feat: card-bar adjust

--- src/app/components/card-bar/card-bar.component.ts
+++ src/app/components/card-bar/card-bar.component.ts
@@ -2,27 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { PlantsService } from '../../plants/plants.service';
 import { Plant } from '../../models/plants.interface';
 import { Subscription } from 'rxjs';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-card-bar',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule, NgxPaginationModule],
   templateUrl: './card-bar.component.html',
   styleUrl: './card-bar.component.scss',
 })
 export class CardBarComponent implements OnInit {
+  p: number = 1;
   plants: Plant[] = [];
   private subscription: Subscription;
   constructor(private plantsService: PlantsService) {
     this.subscription = this.plantsService
       .findAll()
       .subscribe((response: Plant[]) => {
-        this.plants = response; 
+        this.plants = response;
       });
   }
-  ngOnInit(): void {
-    console.log(this.plants);
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
