e6f9599384af8caf86d224390cb39cbab6514472
allan.vargens <allanvargens@gmail.com>
2024-08-07 21:24:18 -0300

    feat: footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CardBarComponent } from './components/card-bar/card-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent, CardBarComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, CardBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
