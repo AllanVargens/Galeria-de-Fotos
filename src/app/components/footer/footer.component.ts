2ebe0e67a67192eb10a8423c6936cfa01bd1f51f
AllanVargens <allanvargens@gmail.com>
2024-08-22 11:16:48 -0300

    feat: image header and footer update

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faEnvelope,
+  faSquarePhone,
+  faLink,
+} from '@fortawesome/free-solid-svg-icons';
+import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  faEnvelope = faEnvelope;
+  faSquarePhone = faSquarePhone;
+  faSquareInstagram = faSquareInstagram;
+  faLink = faLink;
 }
